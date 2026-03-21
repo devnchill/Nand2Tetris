0afcf82917a03bc7d9cdfa985bb36443156ae47d
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-21 13:44:54 +0530

    chore : add devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          nixfmt
+
+          go
+          gopls
+        ];
+        shellHook = ''
+          			export GOPATH="$HOME/.local/share/go"
+          		    export PATH="$PATH:$GOPATH/bin"
+          		'';
+      };
+    };
+}
