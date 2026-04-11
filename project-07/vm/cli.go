60b751955aa6b9c9179dec27e65a95a1cf346ce2
Viena <169875752+devnchill@users.noreply.github.com>
2026-04-11 15:58:32 +0530

    vm1 : refactor main func

---
+++ project-07/vm/cli.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func parseArgs(args []string) ([]string, []string) {
+	if len(args) < 2 {
+		fmt.Println("Usage: vm -f <file.vm ...> -d <directory ...>")
+		os.Exit(1)
+	}
+
+	var fileFlag, dirFlag bool
+	var dirFlagIdx int
+
+	if args[1] == "-f" {
+		fileFlag = true
+		for i, arg := range args[2:] {
+			if arg != "-d" {
+				continue
+			}
+			if i == len(args)-1 {
+				fmt.Println("invalid syntax")
+				fmt.Println("no directory provided")
+				os.Exit(1)
+			}
+			dirFlagIdx = i
+			dirFlag = true
+			break
+		}
+	} else if args[1] == "-d" {
+		dirFlag = true
+	} else {
+		fmt.Println("invalid syntax")
+		os.Exit(1)
+	}
+
+	var files, directories []string
+
+	if fileFlag && !dirFlag {
+		files = args[2:]
+	} else if !fileFlag && dirFlag {
+		directories = args[2:]
+	} else {
+		files = args[2 : dirFlagIdx-1]
+		directories = args[dirFlagIdx+1:]
+	}
+
+	return files, directories
+}
--- project-07/vm/main.go
+++ project-07/vm/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -12,69 +10,26 @@ func main() {
 		fmt.Println("Usage: vm -f <file.vm ...> -d <directory ...>")
 		os.Exit(1)
 	}
-	var fileFlag, dirFlag bool
-	var dirFlagIdx int
 
-	// NOTE: extract it into a helper function
-	if os.Args[1] == "-f" {
-		fileFlag = true
-		for i := range os.Args[2:] {
-			if os.Args[i] != "-d" {
-				continue
-			}
-			if i == len(os.Args)-1 {
-				fmt.Println("invalid syntax")
-				fmt.Println("no directory provided")
-				fmt.Println("Usage: vm -f <file.vm> -d <directory>")
-				os.Exit(1)
-			}
-			dirFlagIdx = i
-			dirFlag = true
-			break
-		}
-	} else if os.Args[1] == "-d" {
-		fileFlag = false
-		dirFlag = true
-	} else {
-		fmt.Println("invalid syntax")
-		fmt.Println("Usage: vm -f <file.vm> -d <directory>")
-		os.Exit(1)
-	}
-	var files, directories []string
-	if fileFlag && !dirFlag {
-		files = os.Args[2:]
-	} else if !fileFlag && dirFlag {
-		directories = os.Args[2:]
-	} else {
-		// both files and directories are provided
-		files = os.Args[2 : dirFlagIdx-1]
-		directories = os.Args[dirFlagIdx+1:]
-	}
-
+	files, directories := parseArgs(os.Args)
 	fmt.Println(files)
 	fmt.Println(directories)
 
-	for _, dir := range directories {
-		err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.IsDir() && filepath.Ext(path) == ".vm" {
-				files = append(files, path)
-			}
-			return nil
-		})
-		if err != nil {
-			fmt.Printf("Error reading directory: %s", dir)
-		}
+	dirFiles, err := collectVMFiles(directories)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	files = append(files, dirFiles...)
+
+	if err := validateVMFiles(files); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	for _, file := range files {
 		fmt.Println(file)
-		if filepath.Ext(file) != ".vm" {
-			fmt.Println("Error: input file must have .vm extension")
-			os.Exit(1)
-		}
 		generateASM(file)
 	}
 }
