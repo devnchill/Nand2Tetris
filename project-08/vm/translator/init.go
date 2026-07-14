4640a291e0294ac002ab39e3fc014eb66a9036dd
Viena <169875752+devnchill@users.noreply.github.com>
2026-07-14 21:09:57 +0530

    fix(boostrap): add \n for new line
    Signed-off-by: Viena <169875752+devnchill@users.noreply.github.com>

--- project-08/vm/translator/init.go
+++ project-08/vm/translator/init.go
@@ -1,9 +1,10 @@
 package translator
 
 func (t *Translator) WriteInit() {
-	t.writer.WriteString("@256")
-	t.writer.WriteString("D=A")
-	t.writer.WriteString("@SP")
-	t.writer.WriteString("M=D")
+	t.writer.WriteString("//bootstrapping code\n")
+	t.writer.WriteString("@256\n")
+	t.writer.WriteString("D=A\n")
+	t.writer.WriteString("@SP\n")
+	t.writer.WriteString("M=D\n")
 	t.WriteCall("Sys.init", 0)
 }
