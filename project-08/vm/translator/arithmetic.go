5e6d2b238a0ccbfa978cfbf22d40f1053c36a836
Viena <theviena96@gmail.com>
2026-07-19 23:07:41 +0530

    refactor(vm II): rewrite switch case for eq,lt and gt
    Signed-off-by: Viena <theviena96@gmail.com>

--- project-08/vm/translator/arithmetic.go
+++ project-08/vm/translator/arithmetic.go
@@ -2,7 +2,13 @@ package translator
 
 import "fmt"
 
-var labelCounter int
+var nextLabel = func() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}()
 
 func (t *Translator) WriteArithmeticCommand(command string) {
 	t.writer.WriteString("\n//" + command + "\n")
@@ -22,33 +28,45 @@ func (t *Translator) WriteArithmeticCommand(command string) {
 		t.writer.WriteString("A=M-1\n")
 		t.writer.WriteString("M=-M\n")
 	case "eq", "gt", "lt":
-		eqLabel := fmt.Sprintf("EQ_%d", labelCounter)
-		endLabel := fmt.Sprintf("END_%d", labelCounter)
-		labelCounter++
-
-		jumpCond := map[string]string{
+		vmToAsmSymol := map[string]string{
 			"eq": "JEQ",
-			"gt": "JGT",
 			"lt": "JLT",
-		}[command]
-
+			"gt": "JGT",
+		}
+		/*
+			@SP -> A = 0 , M = RAM[0]
+			AM = M-1 -> A = RAM[0] - 1 , M = RAM[0]-1
+			hence A pointing to topmost elemnt of stack
+			RAM[SP] pointing to topmost elemnt of stack
+		*/
+		symbolicLabel := fmt.Sprintf("%s_%d", vmToAsmSymol[command], nextLabel())
+		endLabel := fmt.Sprintf("END_%d", nextLabel())
 		t.writer.WriteString("@SP\n")
 		t.writer.WriteString("AM=M-1\n")
 		t.writer.WriteString("D=M\n")
 		t.writer.WriteString("A=A-1\n")
 		t.writer.WriteString("D=M-D\n")
-		t.writer.WriteString("@" + eqLabel + "\n")
-		t.writer.WriteString("D;" + jumpCond + "\n")
+
+		t.writer.WriteString("@" + symbolicLabel + "\n")
+		t.writer.WriteString("D;" + vmToAsmSymol[command] + "\n")
+
+		// if they are not equal
 		t.writer.WriteString("@SP\n")
 		t.writer.WriteString("A=M-1\n")
 		t.writer.WriteString("M=0\n")
 		t.writer.WriteString("@" + endLabel + "\n")
 		t.writer.WriteString("0;JMP\n")
-		t.writer.WriteString("(" + eqLabel + ")" + "\n")
+
+		// define equal label
+		t.writer.WriteString("(" + symbolicLabel + ")\n")
 		t.writer.WriteString("@SP\n")
 		t.writer.WriteString("A=M-1\n")
 		t.writer.WriteString("M=-1\n")
-		t.writer.WriteString("(" + endLabel + ")" + "\n")
+		t.writer.WriteString("@" + endLabel + "\n")
+		t.writer.WriteString("0;JMP\n")
+
+		// define end label
+		t.writer.WriteString("(" + endLabel + ")\n")
 	case "and":
 		{
 			t.writer.WriteString("@SP\n")
